7929eac9eb790b32433941cff721c433f512ed1c
Code Sandbox <hello@codesandbox.io>
2022-12-06 05:50:02 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e61ace4be74a68fbe7495191325d32a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05494f2862f3f2414fdaa0e0752a2300",
+    "url": "https://cmsundara.github.io/csb-ycmi9l/index.html"
+  },
+  {
+    "revision": "48be88e4f2c4c19169c2",
+    "url": "https://cmsundara.github.io/csb-ycmi9l/static/css/main.72dbce2d.chunk.css"
+  },
+  {
+    "revision": "968f6afad006d8ac9673",
+    "url": "https://cmsundara.github.io/csb-ycmi9l/static/js/2.c4d6faee.chunk.js"
+  },
+  {
+    "revision": "ad6dcd34068e967b38fb4ba0a740b6a7",
+    "url": "https://cmsundara.github.io/csb-ycmi9l/static/js/2.c4d6faee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48be88e4f2c4c19169c2",
+    "url": "https://cmsundara.github.io/csb-ycmi9l/static/js/main.ef3305a8.chunk.js"
+  },
+  {
+    "revision": "5f18e96f53eeb9593bd2",
+    "url": "https://cmsundara.github.io/csb-ycmi9l/static/js/runtime-main.ee8350f5.js"
+  }
+]);
